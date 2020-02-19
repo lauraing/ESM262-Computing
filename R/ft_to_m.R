61cf8e5d0e3c67be84e26b71678fe241e27b8163
Laura Ingulsrud <lgi@mac.com>
2020-02-19 12:56:25 -0800

    edited assignments folder

--- R/ft_to_m.R
+++ R/ft_to_m.R
@@ -1,7 +1,7 @@
 # Create function that converts feet to meters
 ft_to_m <- function(ft) {
-	# make sure inputs are positive
-	if (ft < 0) return (NA)
+	# if input is negative, return "ft must be greater than 0"
+	ft = ifelse((ft < 0), return("ft must be greater than zero"), ft)
 	# calculate conversion
 	result = ft * 0.3048
 	return(result)
