c6b4e0e81b550aa4e70fd4992625c3746de9e2a2
Laura Ingulsrud <lgi@mac.com>
2020-02-24 10:38:38 -0800

    updated stream_power.R with two different types of error checking

--- R/stream_power.R
+++ R/stream_power.R
@@ -1,9 +1,9 @@
 # Create stream power function
 stream_power = function(Q, S, rho=1000, g=9.8) {
-  # if inputs are negative, return message "[input] must be greater than zero"
+  # if inputs are negative, return message "[input] must be greater than zero" using two different kinds of error checking 
   Q = ifelse((Q < 0), return("discharge must be greater than zero"), Q)
   S = ifelse((S < 0), return("channel slope must be greater than zero"), S)
-  rho = ifelse((rho < 0), return ("rho must be greater than zero"), rho)
+  if (rho < 0) return("rho must be greater than zero")
   # calculate stream power
   result = rho * g * Q * S
   return(result)
