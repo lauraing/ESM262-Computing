6b08e5ac8ff572f63e0b1df32465187c7428ff8e
Keene Morrow <keene.morrow@gmail.com>
2020-02-24 10:39:40 -0800

    "Contract" added

--- R/stream_power.R
+++ R/stream_power.R
@@ -1,3 +1,14 @@
+#' Stream Power
+#'
+#' This function determines the power of a stream
+#' 
+#' @param Q stream discharge (m^3/s)
+#' @param S channel slope
+#' @param rho water density (g/m^3) default = 1000
+#' @param g acceleration due to gravity (m/s^2) default = 9.8
+#'
+#' @return stream power (W)
+
 # Create stream power function
 stream_power = function(Q, S, rho=1000, g=9.8) {
   # if inputs are negative, return message "[input] must be greater than zero"
