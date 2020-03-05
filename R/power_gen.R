bcf209145b63bc3cc2851548e7236a7069001ebe
Laura Ingulsrud <lgi@mac.com>
2020-03-05 13:32:36 -0800

    Create assignment 2

--- R/power_gen.R
+++ R/power_gen.R
@@ -1,9 +1,9 @@
 # Create power generation function
 power_gen = function(height, flow, rho=1000, g=9.8, Keff=0.8) {
-	# make sure inputs are positive
-	if (height < 0) return (NA)
-	if (flow < 0) return (NA)
-	if (rho < 0) return (NA)
+	# if inputs are negative, return message "[input] must be greater than zero"
+  height = ifelse((height < 0), return("height must be greater than zero"), height)
+  flow = ifelse((flow < 0), return("flow must be greater than zero"), flow)
+  rho = ifelse((rho < 0), return ("rho must be greater than zero"), rho)
 	# calculate power
 	result = rho * height * flow * g * Keff
 	return(result)
